0deaec43cc42c587a1a36c310025b70f6d6443ab
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-17 16:27:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,16 @@
-collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collection(collection) do |collection|
-  yield
-  collection.split(" ").first
+list = ["Tim Jones", "Bob Costas", "Don Knotts"]
+
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
 end
+
+my_collect(list) {|i| i.split(" ").first}
